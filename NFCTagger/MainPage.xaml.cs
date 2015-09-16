e79c6f0ac7f01cf5ad8e932ba129726cf1548c55
m3talelite <Jelle@Braat.NU>
2015-09-16 15:46:48 +0200

    Splitview added

--- NFCTagger/MainPage.xaml.cs
+++ NFCTagger/MainPage.xaml.cs
@@ -25,6 +25,54 @@ namespace NFCTagger
         public MainPage()
         {
             this.InitializeComponent();
+            ListView bla = DatabindingExampleListView();
+            List<String> bla2 = DatabindingExampleList();
+
+            svItemList.Items.Add(bla);
+        }
+
+        private ListView DatabindingExampleListView()
+        {
+            ListView list = new ListView();
+            list.Items.Add("Item 0");
+            list.Items.Add("Item 1");
+            list.Items.Add("Item 2");
+            list.Items.Add("Item 3");
+            list.Items.Add("Item 4");
+
+            list.SelectionChanged += List_SelectionChanged;
+
+            return list;
+        }
+        private List<string> DatabindingExampleList()
+        {
+            List<String> list = new List<string>();
+            list.Add("Item 0");
+            list.Add("Item 1");
+            list.Add("Item 2");
+            list.Add("Item 3");
+            list.Add("Item 4");
+            
+            return list;
+        }
+
+        private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SolidColorBrush blueBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(1, 0, 0, 1));
+            this.Background = blueBrush;
+        }
+
+        private void svItemList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            SolidColorBrush blueBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(1,0,0,1));
+            this.Background = blueBrush;
+            var obj = (ListView)sender
+//            tbContent.Text = 
+        }
+
+        private void btHamburger_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
         }
     }
 }
---
+++ NFCTagger/Model/NavigationItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFCTagger.Model
+{
+    class NavigationItem
+    {
+        public string name { get; set; }
+
+
+        public NavigationItem(string nam)
+        {
+            this.name = nam;
+        }
+    }
+}
